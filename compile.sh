bf7e3b7ed336d2274751655162894e0c989a9855
qikcik <qikcik@gmail.com>
2019-05-22 19:52:06 +0200

    backup

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#bin/bash
+
+cd make
+	
+echo -e "\e[39m -------- \e[31mBUILDING \e[39m--------"
+cmake -G "Ninja"
+ninja
+
+cd ..
---
+++ run.sh
@@ -0,0 +1,8 @@
+#bin/bash
+
+./compile.sh 
+
+echo -e "\e[39m -------- \e[32mRUNNING \e[39m --------" 
+
+chmod +x build/LD43 
+./build/LD43
